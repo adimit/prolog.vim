6aa65a11df2fcaae796a40d9502b5691a0f2183a
Александър Димитров <aleks_d@gmx.de>
2008-10-02 23:53:24 +0200

    Added a testcase

--- test.pl
+++ test.pl
@@ -1,11 +1,12 @@
 % A small Prolog file to test prolog.vim
 
+head :- body.
+
 :- statement.
 :- statement(x,y,X,Y,foo(x,Y)).
 ?- query.
 ?- query(x,y,X,Y,foo(x,Y)).
 
-head :- body.
 head :- body(foo,Foo,foo(foo)).
 
 head(booh,X,foo(booh,X)) :- body.
@@ -21,13 +22,14 @@ head(booh,X,foo(booh,X)) --> foo,
 dynamic foo/2.
 
 foobar.
-foobar :- !, \+ , >>.
+
+foobar :- !, \+ ,  >>.
 
 head :- foo(C), foo(B),
 	/* something commentary */
 	% more comments
 	\+ foo(G).
-
+ 
 /* a multiline
 * comment
 */
@@ -36,7 +38,7 @@ head :- Error(foo). /*predicates must start lower-case*/
 
 head :- error(1) error(2). /*no comma and a TODO*/
 
-head :- body.
+head :- body, Ts =.. [Foo|Bar].
 
 head :- list([a,b,C,foo:Bar|Tail],Foo).
 head :- dlist(H-Ts).
